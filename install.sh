d66d3c82ddbfb8f9e6f91a14bd97e68e9b11b24d
joshpfosi <joshpfosi@arista.com>
2020-08-21 12:49:32 -0700

    Fix install script

--- install.sh
+++ install.sh
@@ -8,17 +8,18 @@ INSTALL_PATH="$(dirname "${BASH_SOURCE[0]}")"
 
 echo "Symlinking from $INSTALL_PATH to $HOME..."
 
-rm -rf $HOME/.vim
-ln -sf $INSTALL_PATH/.vim $HOME/.vim
 ln -sf $INSTALL_PATH/.vimrc $HOME/.vimrc
 ln -sf $INSTALL_PATH/.bashrc $HOME/.bashrc
 ln -sf $INSTALL_PATH/.tmux.conf $HOME/.tmux.conf
 ln -sf $INSTALL_PATH/.bash_profile $HOME/.bash_profile
 ln -sf $INSTALL_PATH/.gitconfig $HOME/.gitconfig
 
+rm -rf $HOME/.vim
+ln -sf $INSTALL_PATH/.vim $HOME/.vim
+
 # VIM plugins
 VUNDLE=$HOME/.vim/bundle/Vundle.vim
-if [ -f $VUNDLE ]
+if [ -e $VUNDLE ]
 then
    echo "$VUNDLE already present; not cloning..."
 else
