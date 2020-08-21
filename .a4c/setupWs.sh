d09b2fc0ef9148f51601f155ede27e20f0bf80aa
joshpfosi <joshpfosi@arista.com>
2020-08-21 09:25:14 -0700

    Clean out all Arista-specific stuff

--- .a4c/setupWs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# taccscope -b -q &
-a4 yum install -y --enablerepo="*" man man-pages colordiff &
-# (
-#    find . -name "*.py" > pycscope.files
-#    pycscope -R -f pycscope.out -i pycscope.files
-#    rm pycscope.files
-# ) &
